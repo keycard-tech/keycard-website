925cde1b96196691a509381b1a89b1490d433d95
Pavel <14926950+prichodko@users.noreply.github.com>
2024-10-21 13:40:28 +0200

    setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import { cx } from 'cva'
+import type { Metadata } from 'next'
+import { Inter, Lora } from 'next/font/google'
+import './globals.css'
+
+const lora = Lora({
+  variable: '--font-lora',
+  weight: '400',
+  subsets: ['latin'],
+  preload: true,
+})
+
+const inter = Inter({
+  variable: '--font-inter',
+  weight: ['300', '400', '500'],
+  subsets: ['latin'],
+  preload: true,
+})
+
+export const metadata: Metadata = {
+  title: 'Keycard',
+  description:
+    'Join the open source revolution of the most modular and future proof hardware wallet system ever conceived.',
+}
+
+type Props = {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang="en">
+      <body
+        className={cx(
+          lora.variable,
+          inter.variable,
+          'bg-dark-100 font-inter text-white-100 antialiased',
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-lora text-48 font-400">Keycard</h1>
+      <a className="rounded-12 bg-orange px-4 py-2 text-16 font-500 text-white-100">
+        Buy Keycard
+      </a>
+    </div>
+  )
+}
