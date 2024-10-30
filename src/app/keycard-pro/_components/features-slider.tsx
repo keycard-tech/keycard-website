13c8870b401d50ce1ba978afc217871c30903b9b
marcelines <marcio@status.im>
2024-10-30 18:44:11 +0000

    f

--- src/app/keycard-pro/_components/features-slider.tsx
+++ src/app/keycard-pro/_components/features-slider.tsx
@@ -39,23 +39,6 @@ const features = [
     description: 'Easily identify your cards by giving them a unique name.',
     image: '/assets/keycard-pro/keycard-pro-feature-card-6.png',
   },
-  {
-    name: 'Compatible QR wallets',
-    description:
-      'Its embedded camera allows Keycard Pro to be used fully airgapped.',
-    image: '/assets/keycard-pro/keycard-pro-feature-card-4.png',
-  },
-  {
-    name: 'Block USB data transfer',
-    description:
-      'Add an extra layer of security by preventing unwanted data transfers.',
-    image: '/assets/keycard-pro/keycard-pro-feature-card-5.png',
-  },
-  {
-    name: 'Name your cards',
-    description: 'Easily identify your cards by giving them a unique name.',
-    image: '/assets/keycard-pro/keycard-pro-feature-card-6.png',
-  },
 ]
 
 const CARD_WIDTH = 320 + 24
