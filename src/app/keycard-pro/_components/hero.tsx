80a5bfbc9558cb593108fdd5ce430b18a9a94108
marcelines <marcio@status.im>
2024-10-31 15:37:46 +0000

    f: heroes

--- src/app/keycard-pro/_components/hero.tsx
+++ src/app/keycard-pro/_components/hero.tsx
@@ -3,33 +3,37 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <section className="relative flex h-screen overflow-y-clip lg:h-[986px]">
+    <section className="relative flex h-[986px] overflow-y-clip">
       <Image
         alt="Keycard Pro"
         src="/assets/keycard-pro/bg-hero-keycard-pro.png"
         width={986}
         height={986}
-        className="absolute bottom-0 left-1/3 top-1/3 z-0 -translate-x-1/2 -translate-y-1/4 bg-contain bg-top bg-no-repeat lg:left-1/3 lg:top-1/3 lg:h-auto lg:w-fit lg:-translate-x-1/2 lg:-translate-y-1/2"
+        priority
+        className="absolute left-1/3 top-1/3 z-0 h-auto w-fit -translate-x-1/2 -translate-y-1/2 bg-contain bg-top bg-no-repeat"
       />
       <div className="absolute bottom-0 left-0 z-10 h-1/3 w-full bg-gradient-to-b from-[transparent] to-dark-100 lg:bottom-40 lg:left-1/2 lg:-translate-x-1/2" />
-      <div className="relative z-20 flex flex-1 justify-between px-20 pb-11 align-bottom lg:pb-[270px]">
-        <div className="flex max-w-[664px] flex-col self-end">
-          <p className="text-24 font-600">
-            keycard <span className="font-200">pro</span>
-          </p>
-          <h1 className="flex font-lora text-48 font-400 text-white-95">
-            Something amazing about great things then something more over here.
-          </h1>
-        </div>
+      <div className="relative z-20 flex flex-1 flex-col justify-end px-20 pb-11 align-bottom lg:pb-[270px]">
+        <p className="pb-2 text-24 font-600">
+          keycard <span className="font-200">pro</span>
+        </p>
+        <div className="flex items-start justify-between">
+          <div className="flex max-w-[664px] flex-col self-end">
+            <h1 className="flex font-lora text-48 font-400 text-white-95">
+              Something amazing about great things then something more over
+              here.
+            </h1>
+          </div>
 
-        <div className="flex max-w-[435px] flex-col justify-end self-end">
-          <p className="pb-12 text-right text-20 font-300 text-white-80">
-            A principle-based hardware wallet that relies on Keycard as a
-            modular secure element
-          </p>
-          <div className="flex items-center justify-end space-x-4">
-            <p className="text-16 font-300 text-white-60">Coming 2025</p>
-            <ButtonLink href="/">Get notified</ButtonLink>
+          <div className="flex max-w-[435px] flex-col self-start pt-3">
+            <p className="pb-12 text-right text-20 font-300 text-white-80">
+              A principle-based hardware wallet that relies on Keycard as a
+              modular secure element
+            </p>
+            <div className="flex items-center justify-end space-x-4">
+              <p className="text-16 font-300 text-white-60">Coming 2025</p>
+              <ButtonLink href="/">Get notified</ButtonLink>
+            </div>
           </div>
         </div>
       </div>
--- src/app/keycard/_components/hero.tsx
+++ src/app/keycard/_components/hero.tsx
@@ -1,28 +1,36 @@
 import { ButtonLink } from '~components/button-link'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <section className="relative flex h-screen">
-      <div
-        className="absolute inset-0 left-1/2 z-0 w-screen -translate-x-1/2 bg-contain bg-top bg-no-repeat"
-        style={{ backgroundImage: 'url(/assets/keycard/hero-keycard.png)' }}
+    <section className="relative flex h-[986px] overflow-y-clip">
+      <Image
+        alt="Keycard Pro"
+        src="/assets/keycard/hero-keycard.png"
+        width={1600}
+        height={1600}
+        priority
+        className="absolute left-1/2 top-1/3 z-0 w-full -translate-x-1/2 -translate-y-1/2 bg-contain bg-center bg-no-repeat"
       />
-      <div className="relative z-10 flex flex-1 justify-between px-20 pb-11 align-bottom">
-        <div className="flex max-w-[664px] flex-col self-end">
-          <p className="pb-2 text-24 font-600 text-orange">keycard</p>
-          <h1 className="flex font-lora text-48 font-400 text-white-95">
-            Something amazing about great things then something more over here.
-          </h1>
-        </div>
+      <div className="relative z-10 flex flex-1 flex-col justify-end px-20 pb-11 align-bottom lg:pb-[270px]">
+        <p className="pb-2 text-24 font-600 text-orange">keycard</p>
+        <div className="flex items-start justify-between">
+          <div className="flex max-w-[664px] flex-col self-end">
+            <h1 className="flex font-lora text-48 font-400 text-white-95">
+              Something amazing about great things then something more over
+              here.
+            </h1>
+          </div>
 
-        <div className="flex max-w-[435px] flex-col justify-end self-end">
-          <p className="pb-12 text-right text-20 font-300 text-white-80">
-            A principle-based hardware wallet that relies on Keycard as a
-            modular secure element
-          </p>
-          <div className="flex items-center justify-end space-x-4">
-            <p className="text-16 font-300 text-white-60">Starts from $25</p>
-            <ButtonLink href="/">Buy Keycard</ButtonLink>
+          <div className="flex max-w-[435px] flex-col self-start pt-3">
+            <p className="pb-12 text-right text-20 font-300 text-white-80">
+              A principle-based hardware wallet that relies on Keycard as a
+              modular secure element
+            </p>
+            <div className="flex items-center justify-end space-x-4">
+              <p className="text-16 font-300 text-white-60">Starts from $25</p>
+              <ButtonLink href="/">Buy Keycard</ButtonLink>
+            </div>
           </div>
         </div>
       </div>
