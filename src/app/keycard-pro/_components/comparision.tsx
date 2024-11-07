7742d5f43091cf9d914ce4ed16a60ceed230e64e
marcelines <marcio@status.im>
2024-11-07 14:51:11 +0000

    f: comparision section

--- src/app/keycard-pro/_components/comparision.tsx
+++ src/app/keycard-pro/_components/comparision.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 const Comparision = () => {
   return (
-    <section className="mt-[200px] grid gap-0 overflow-clip rounded-28 border border-white-12 md:grid-cols-2">
-      <div className="relative flex min-h-[840px] flex-col items-center justify-center">
+    <section className="mt-[120px] grid gap-0 overflow-clip rounded-28 border border-white-12 md:grid-cols-2 lg:mt-[200px]">
+      <div className="relative flex min-h-[480px] flex-col items-center justify-center lg:min-h-[840px]">
         <Image
           alt="Keycard Pro"
           className="absolute left-0 top-0 z-0 size-full object-cover"
@@ -11,16 +11,16 @@ const Comparision = () => {
           layout="fill"
         />
 
-        <h2 className="absolute left-1/2 top-1/2 z-20 w-full -translate-x-1/2 -translate-y-20 text-center font-lora text-32">
+        <h2 className="absolute left-1/2 top-1/2 z-20 mx-auto w-full max-w-[328px] -translate-x-1/2 -translate-y-20 text-center font-lora text-32 lg:max-w-full">
           Keep your device at home
         </h2>
-        <p className="absolute bottom-14 mx-auto max-w-[423px] px-2 text-center text-16 font-300 text-white-80">
+        <p className="absolute bottom-8 mx-auto max-w-[423px] px-2 text-center text-16 font-300 text-white-80 lg:bottom-14">
           Stored away on a drawer, or hiding in plain sight, Keycard Pro is
           stateless without a Keycard.
         </p>
       </div>
 
-      <div className="relative flex min-h-[840px] flex-col items-center justify-center border-t border-white-12 md:border-l md:border-t-0">
+      <div className="relative flex min-h-[480px] flex-col items-center justify-center border-t border-white-12 md:border-l md:border-t-0 lg:min-h-[840px]">
         <Image
           alt="Keycard"
           className="absolute left-0 top-0 size-full object-cover"
@@ -28,10 +28,10 @@ const Comparision = () => {
           layout="fill"
         />
 
-        <h2 className="absolute left-1/2 top-1/2 z-20 w-full -translate-x-1/2 -translate-y-20 text-center font-lora text-32">
+        <h2 className="absolute left-1/2 top-1/2 z-20 mx-auto w-full max-w-[328px] -translate-x-1/2 -translate-y-20 text-center font-lora text-32 lg:max-w-full">
           Take your keys with you
         </h2>
-        <p className="absolute bottom-14 mx-auto max-w-[434px] px-2 text-center text-16 font-300 text-white-80">
+        <p className="absolute bottom-8 mx-auto max-w-[434px] px-2 text-center text-16 font-300 text-white-80 lg:bottom-14">
           Discreet and light, your Keycard fits perfectly in your wallet ready
           to go, everywhere!
         </p>
