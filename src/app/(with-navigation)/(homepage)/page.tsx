a501cc7430931df60c3c5e4df94624f2060ee3b4
marcelines <marcio@status.im>
2024-11-22 09:56:12 +0000

    f: removes unnecessary dialog

--- src/app/(with-navigation)/(homepage)/page.tsx
+++ src/app/(with-navigation)/(homepage)/page.tsx
@@ -4,12 +4,10 @@ import { Faqs } from '~/app/(with-navigation)/(homepage)/_components/faqs'
 import { Hero } from '~/app/(with-navigation)/(homepage)/_components/hero'
 import { KeycardFeatures } from '~/app/(with-navigation)/(homepage)/_components/keycard-features'
 import { KeycardShell } from '~/app/(with-navigation)/(homepage)/_components/keycard-shell'
-import { ThankYouDialog } from './_components/thank-you-dialog'
 
 export default async function HomePage() {
   return (
     <>
-      <ThankYouDialog />
       <Hero />
       <KeycardShell />
       <KeycardFeatures />
