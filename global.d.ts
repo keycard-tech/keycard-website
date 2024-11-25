2227b9becf8457e5e7dbdbdd85056d2508978ab5
Felicio Mununga <felicio@users.noreply.github.com>
2024-11-25 11:05:28 +0100

    u

--- global.d.ts
+++ global.d.ts
@@ -1,17 +1,7 @@
-// why: https://react.dev/blog/2024/04/25/react-19-upgrade-guide#the-jsx-namespace-in-typescript
-// declare module 'react' {
-//   namespace JSX {
-//     interface IntrinsicElements {
-//       'my-element': {
-//         myElementProps: string
-//       }
-//     }
-//   }
-// }
-
 import type * as React from 'react'
 
 // why: https://github.com/mdx-js/mdx/issues/2487#issuecomment-2407661869
+// why: https://react.dev/blog/2024/04/25/react-19-upgrade-guide#the-jsx-namespace-in-typescript
 declare global {
   namespace JSX {
     // type ElementClass = React.JSX.ElementClass
