3ae3163f974a6be34ead85521e4220f8375552a9
Jakub Kotula <520927+jkbktl@users.noreply.github.com>
2024-11-26 15:36:53 +0100

    f footer / prefooter

--- src/app/_components/footer/index.tsx
+++ src/app/_components/footer/index.tsx
@@ -6,18 +6,27 @@ import { Section } from './section'
 const Footer = () => {
   return (
     <footer className="rounded-28 bg-orange pt-8">
-      <div className="grid grid-cols-2 divide-x divide-dashed divide-white-20 lg:grid-cols-6">
-        <div className="flex items-start border-b border-dashed border-white-20 p-6 pr-0 pt-0">
+      <div className="relative">
+        <div className="absolute flex items-start pl-6">
           <Link href="/" aria-label="Homepage">
             <Logo />
           </Link>
         </div>
-        {Object.entries(ROUTES).map(([title, links]) => (
-          <Section key={title} title={title} routes={links} />
-        ))}
+        <div className="m-auto max-w-[1512px]">
+          <div className="grid grid-cols-2 divide-x divide-dashed divide-white-20 lg:grid-cols-5 lg:pl-[152px] lg:pr-[72px] xl:pl-[282px]">
+            <div
+              key="empty"
+              title="empty"
+              className="border-b border-dashed border-white-20 lg:hidden"
+            />
+            {Object.entries(ROUTES).map(([title, links]) => (
+              <Section key={title} title={title} routes={links} />
+            ))}
+          </div>
+        </div>
       </div>
 
-      <div className="flex flex-col items-center justify-between gap-2 px-5 py-4 md:flex-row md:gap-0 lg:px-6">
+      <div className="flex flex-col items-center justify-between gap-2 border-0 border-dashed border-white-20 px-5 py-4 md:flex-row md:gap-0 lg:border-t lg:px-6">
         <span className="text-16 font-300 text-white-80">
           © Keycard {new Date().getFullYear()}
         </span>
