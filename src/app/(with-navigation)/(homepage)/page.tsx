c8d2f56f0c72e4d6d8bd1063d5eb79b078923780
Felicio Mununga <felicio@users.noreply.github.com>
2024-11-27 13:44:24 +0100

    f

--- src/app/(with-navigation)/(homepage)/page.tsx
+++ src/app/(with-navigation)/(homepage)/page.tsx
@@ -4,11 +4,14 @@ import { Faqs } from '~/app/(with-navigation)/(homepage)/_components/faqs'
 import { Hero } from '~/app/(with-navigation)/(homepage)/_components/hero'
 import { KeycardFeatures } from '~/app/(with-navigation)/(homepage)/_components/keycard-features'
 import { KeycardShell } from '~/app/(with-navigation)/(homepage)/_components/keycard-shell'
+import { Suspense } from 'react'
 
 export default function HomePage() {
   return (
     <>
-      <Hero />
+      <Suspense>
+        <Hero />
+      </Suspense>
       <KeycardShell />
       <KeycardFeatures />
       <ComparisonTable />
