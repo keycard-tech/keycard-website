8af5fdb3f279b0f9b2989e753d3d7c5d2546172c
marcelines <marcio@status.im>
2024-12-02 11:40:30 +0000

    f: keycard shell section

--- src/app/(with-navigation)/(homepage)/_components/keycard-shell.tsx
+++ src/app/(with-navigation)/(homepage)/_components/keycard-shell.tsx
@@ -4,44 +4,35 @@ import { Image } from '~components/image'
 
 const KeycardShell = () => {
   return (
-    <section className="relative mx-0 mt-[160px] flex items-center overflow-hidden rounded-28 border border-white-12 lg:mx-6 lg:mt-[200px] lg:justify-end xl:mx-20">
+    <section className="relative mx-0 mt-[160px] flex flex-col items-center overflow-hidden rounded-28 border border-white-12 lg:mx-6 lg:mt-[200px] lg:flex-row lg:justify-end xl:mx-20">
       <Image
         src="/assets/bg-keycard-v1.png"
         alt="Keycard Shell Hardware Wallet"
         width={1400}
         height={800}
-        className="absolute left-0 top-0 hidden size-full object-cover md:block"
+        className="absolute left-0 top-0 hidden size-full object-cover lg:block"
       />
 
       <Image
-        src="/assets/bg-keycard-v1.png"
+        src="/assets/bg-keycard-mobile-v1.png"
         alt="Keycard Shell Hardware Wallet"
-        width={828}
-        height={1452}
-        className="absolute left-0 top-0 block size-full scale-[101%] object-cover md:hidden"
+        width={414}
+        height={614}
+        className="relative left-0 top-0 block aspect-[414/614] w-full lg:hidden"
       />
 
-      <div className="relative z-10 flex flex-col p-6 pt-[400px] lg:py-[200px] lg:pr-[229px]">
-        <p className="pb-3 text-24 font-600 text-white-95">
-          keycard <span className="font-200">shell</span>
-        </p>
-        <p className="flex pb-2 font-lora text-32 font-400">
-          Multiple Keycards.
+      <div className="relative z-10 mt-[-53%] flex w-full flex-col p-6 pt-0 lg:mt-0 lg:w-auto lg:py-[256px] lg:pr-[229px]">
+        <p className="flex pb-4 font-lora text-32 font-400">
+          Get ready for
           <br />
-          One device.
+          the future.
         </p>
-        <p className="max-w-[320px] pb-6 text-16 font-300 text-white-60">
-          Use the same device for all your Keycards. It’s airgaped, stateless
-          and modular.
+        <p className="max-w-[320px] pb-8 text-16 font-300 text-white-60">
+          Revealing soon. Coming 2025
         </p>
-
         <GetNotifiedDialog>
           <Button>Get notified</Button>
         </GetNotifiedDialog>
-
-        <p className="pt-6 text-16 font-300 text-white-60">
-          Revealing soon. Coming 2025
-        </p>
       </div>
     </section>
   )
