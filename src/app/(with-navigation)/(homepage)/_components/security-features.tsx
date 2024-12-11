d0de29dd0e19efc3413b008d1f348e72885aa7fb
marcelines <marcio@status.im>
2024-12-11 16:34:39 +0000

    f: keycard image

--- src/app/(with-navigation)/(homepage)/_components/security-features.tsx
+++ src/app/(with-navigation)/(homepage)/_components/security-features.tsx
@@ -9,12 +9,13 @@ const SecurityFeatures = () => {
     <section className="relative mt-[100px] overflow-clip px-3 lg:mt-[227px] lg:px-0">
       <div className="rounded-28 border border-white-8 bg-white-4 px-6 md:px-0">
         <div className="flex flex-col items-center justify-between lg:flex-row">
-          <div className="-mx-6 flex-1 self-start lg:mx-0">
+          <div className="relative -mx-6 flex-1 self-start lg:mx-0">
+            <div className="absolute -left-1/2 top-0 z-0 h-[390px] w-[1198px] translate-y-[-190px] -rotate-45 bg-gradient-to-b from-dark-100 to-[transparent]" />
             <Image
-              src="/assets/keycard/security.png"
+              src="/assets/keycard/chip.png"
               alt="Keycard security"
-              width={680}
-              height={695}
+              width={580}
+              height={736}
               priority
             />
           </div>
