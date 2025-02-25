0382875988236632ed92efd76778b5d7c08a5239
Jakub Kotula <520927+jkbktl@users.noreply.github.com>
2025-02-25 16:05:46 +0100

    f

--- src/app/(with-navigation)/keycard-shell/_components/hero.tsx
+++ src/app/(with-navigation)/keycard-shell/_components/hero.tsx
@@ -40,6 +40,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
+      <div className="absolute inset-x-0 bottom-0 z-10 h-1/3 bg-gradient-to-t from-[#010101] to-[transparent]" />
     </section>
   )
 }
