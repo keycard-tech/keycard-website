c69ccfca1bcbb76a5b80b1e5864bf3bb86cd43c2
JulesFILIOT <dev@julesfiliot.com>
2025-04-28 18:51:11 +0200

    chore: update strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({ children }: Props) {
         </Providers>
         <Analytics />
         <Script
-          defer
+          strategy="afterInteractive"
           src="https://umami.bi.status.im/script.js"
           data-website-id="a335ad8b-deef-4960-b565-3d4e21b7a8e5"
           data-domains="keycard.tech"
