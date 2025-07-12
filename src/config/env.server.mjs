31ebcbd549e9414ce07fd977920c5396f1027c08
Felicio <felicio@users.noreply.github.com>
2025-07-12 15:23:55 +0900

    handle env vars

---
+++ src/config/env.server.mjs
@@ -0,0 +1,38 @@
+import { z } from 'zod'
+import { handleError } from './env.base.mjs'
+
+if (typeof window !== 'undefined') {
+  throw new Error(
+    '❌ Attempted to access a server-side environment variable on the client',
+  )
+}
+
+export const envSchema = z.object({
+  VERCEL: z.string().optional(),
+  VERCEL_ENV: z
+    .union([
+      z.literal('production'),
+      z.literal('preview'),
+      z.literal('development'),
+    ])
+    .optional(),
+  SHOPIFY_STORE_DOMAIN: z.string(),
+  SHOPIFY_STOREFRONT_API_PUBLIC_ACCESS_TOKEN: z.string(),
+  SHOPIFY_ADMIN_API_PRIVATE_ACCESS_TOKEN: z.string(),
+  GOOGLE_SPREADSHEET_ID: z.string(),
+  GOOGLE_SHEET_ID: z.string(),
+  GOOGLE_CLIENT_EMAIL: z.string(),
+  GOOGLE_SERVICE_PRIVATE_KEY: z.string(),
+})
+
+const result = envSchema.safeParse(process.env)
+
+if (!result.success) {
+  handleError(result.error)
+
+  process.exit(1)
+}
+
+const serverEnv = result.data
+
+export { serverEnv }
