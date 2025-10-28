d60b08423a555fd711fb2a86562bf80d6f79f464
Jakub Kotula <520927+jkbktl@users.noreply.github.com>
2025-10-28 23:01:33 +0100

    update footer

--- src/app/_components/footer/section.tsx
+++ src/app/_components/footer/section.tsx
@@ -11,12 +11,10 @@ const Section = (props: Props) => {
   const { title, routes } = props
 
   return (
-    <div className="relative flex flex-col gap-6 border-b border-dashed border-white-20 p-6 lg:border-0 lg:pb-12 lg:pt-0 lg:[&:nth-child(1)]:col-start-2">
+    <div className="relative flex flex-col gap-6 border-b border-dashed border-white-20 p-6 lg:border-0 lg:pb-12 lg:pt-0 lg:[&:nth-child(1)]:col-start-2 [&:nth-child(6)]:col-span-2 lg:[&:nth-child(6)]:col-span-1 [&:nth-child(odd)]:border-l">
       <p className="text-12 uppercase text-white-80">{title}</p>
       <ul className="grid gap-2">
         {routes.map(route => {
-          const external = route.href.startsWith('http')
-
           return (
             <li key={route.name}>
               <Link
@@ -26,7 +24,9 @@ const Section = (props: Props) => {
                 <span className="text-16 font-500 text-white-95 transition-colors group-hover:text-white-60 group-data-[active='true']:text-white-40">
                   {route.name}
                 </span>
-                {external && <ExternalIcon className="text-white-60" />}
+                {'external' in route && route.external && (
+                  <ExternalIcon className="text-white-60" />
+                )}
               </Link>
             </li>
           )
