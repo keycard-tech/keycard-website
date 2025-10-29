62daa102ff874f146f215947d2edeecf0eb75cde
Jakub Kotula <520927+jkbktl@users.noreply.github.com>
2025-10-29 15:08:58 +0100

    fix classname

--- src/app/_components/footer/section.tsx
+++ src/app/_components/footer/section.tsx
@@ -1,6 +1,7 @@
 import { ExternalIcon } from '@status-im/icons/20'
 import { Routes } from '~/config/routes'
 import { Link } from '~components/link'
+import { cx } from 'cva'
 
 type Props = {
   title: string
@@ -11,7 +12,13 @@ const Section = (props: Props) => {
   const { title, routes } = props
 
   return (
-    <div className="relative flex flex-col gap-6 border-b border-dashed border-white-20 p-6 lg:border-0 lg:pb-12 lg:pt-0 lg:[&:nth-child(1)]:col-start-2 [&:nth-child(6)]:col-span-2 lg:[&:nth-child(6)]:col-span-1 [&:nth-child(odd)]:border-l">
+    <div
+      className={cx(
+        'relative flex flex-col gap-6 border-b border-dashed border-white-20 p-6',
+        'lg:border-0 lg:pb-12 lg:pt-0 lg:[&:nth-child(1)]:col-start-2 lg:[&:nth-child(6)]:col-span-1',
+        '[&:nth-child(6)]:col-span-2 [&:nth-child(odd)]:border-l',
+      )}
+    >
       <p className="text-12 uppercase text-white-80">{title}</p>
       <ul className="grid gap-2">
         {routes.map(route => {
